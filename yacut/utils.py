81c49c8d024090fd6800a3b14bfd5d88972e7005
AlxShvalev <shvalevalx@gmail.com>
2022-08-02 23:41:37 +0500

    refactoring get_unique_short_id

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,8 +1,16 @@
 import random
 import string
 
+from .models import URL_map
 
-def generate_rand_string(length):
+
+def get_random_string(length: int) -> str:
     characters = string.ascii_letters + string.digits
-    rand_string = ''.join(random.choices(characters, k=length))
+    return ''.join(random.choices(characters, k=length))
+
+
+def get_unique_short_id(length: int) -> str:
+    rand_string = get_random_string(length)
+    while URL_map.query.filter_by(short=rand_string).first() is not None:
+        rand_string = get_random_string(length)
     return rand_string
