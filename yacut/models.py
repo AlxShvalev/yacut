1a0e9fa5953f4e0e5f428c1d7e6bca199cd3fe3c
AlxShvalev <shvalevalx@gmail.com>
2022-08-02 23:37:54 +0500

    added get_original_url

--- yacut/models.py
+++ yacut/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import request
 
 from yacut import db
 
@@ -8,9 +9,20 @@ class URL_map(db.Model):
     original = db.Column(db.String, nullable=False)
     short = db.Column(db.String, nullable=False, unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    fields = ['original', 'short']
 
-    def to_dict(self):
-        pass
+    def to_dict(self) -> dict:
+        return dict(
+            url=self.original,
+            short_link=request.host_url + self.short,
+        )
 
-    def from_dict(self):
-        pass
+    def from_dict(self, data: dict) -> None:
+        for field in self.fields:
+            if field in data:
+                setattr(self, field, data[field])
+
+    def get_original_url(self) -> dict:
+        return dict(
+            url=self.original
+        )
