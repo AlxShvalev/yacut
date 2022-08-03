1de7e551c8b3d17e03a1f1472f91d220bd2d60d4
AlxShvalev <shvalevalx@gmail.com>
2022-08-03 20:49:53 +0500

    add connection to db in settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = (os.getenv('DATABASE_URI')
+                               or 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'SECRET_KEY'
