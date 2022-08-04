9e65b3f36d48a5f2bf471fd13444a19f7352f1cf
AlxShvalev <shvalevalx@gmail.com>
2022-08-04 20:11:55 +0500

    use first_or_404 in get_original_url_view

--- yacut/views.py
+++ yacut/views.py
@@ -1,4 +1,4 @@
-from flask import abort, flash, redirect, request, render_template, typing
+from flask import flash, redirect, render_template, typing
 from http import HTTPStatus
 
 from . import app, db, SHORT_LINK_LEN
@@ -16,30 +16,27 @@ def index() -> typing.ResponseReturnValue:
 def create_short_url_view() -> typing.ResponseReturnValue:
     """Создает новое сокращение в базе."""
     form = URLForm()
-    if form.validate_on_submit():
-        original = form.original_link.data
-        short = form.custom_id.data
-        if short is None:
-            short = get_unique_short_id(SHORT_LINK_LEN)
-        if URL_map.query.filter_by(short=short).first() is not None:
-            flash(f'Имя {short} уже занято!')
-            return render_template('index.html', form=form), HTTPStatus.OK
-        url = URL_map(
-            original=original,
-            short=short,
-        )
-        db.session.add(url)
-        db.session.commit()
-        short_link = request.host_url + short
-        return (render_template('index.html', form=form, url=short_link),
-                HTTPStatus.OK)
-    return render_template('index.html', form=form), HTTPStatus.OK
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form), HTTPStatus.OK
+    original = form.original_link.data
+    short = form.custom_id.data
+    if short is None:
+        short = get_unique_short_id(SHORT_LINK_LEN)
+    if URL_map.query.filter_by(short=short).first() is not None:
+        flash(f'Имя {short} уже занято!')
+        return render_template('index.html', form=form), HTTPStatus.OK
+    url = URL_map(
+        original=original,
+        short=short,
+    )
+    db.session.add(url)
+    db.session.commit()
+    return (render_template('index.html', form=form, url=url.get_short_url()),
+            HTTPStatus.OK)
 
 
 @app.route('/<string:short_id>', methods=['GET'])
 def get_original_url_view(short_id: str) -> typing.ResponseReturnValue:
     """Перенапраялвет на оригинальный URL по короткому"""
-    url_map = URL_map.query.filter_by(short=short_id).first()
-    if url_map is not None:
-        return redirect(url_map.original, code=HTTPStatus.FOUND)
-    abort(HTTPStatus.NOT_FOUND)
+    url_map = URL_map.query.filter_by(short=short_id).first_or_404()
+    return redirect(url_map.original, code=HTTPStatus.FOUND)
