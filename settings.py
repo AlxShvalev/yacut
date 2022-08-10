3d61848a30c421c1d76a0e152b4c35fc75247784
AlxShvalev <shvalevalx@gmail.com>
2022-08-10 19:21:50 +0500

    refactor default values

--- settings.py
+++ settings.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = (os.getenv('DATABASE_URI') or
-                               'sqlite:///db.sqlite3')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'SECRET_KEY'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
